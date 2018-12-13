9e727aa2ead9a6c4935b2c9173f3a6b9a497159a
Harry A.M <harry.andriyan@gmail.com>
2018-12-13 20:38:45 +0700

    add weatther blank page

--- src/AppBundle/Controller/CityController.php
+++ src/AppBundle/Controller/CityController.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class CityController extends Controller
 {
--- src/AppBundle/Controller/WeatherController.php
+++ src/AppBundle/Controller/WeatherController.php
@@ -2,8 +2,20 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class WeatherController extends Controller
 {
+    /**
+     * @Route("/weather", name="weather")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('weather.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
 }
