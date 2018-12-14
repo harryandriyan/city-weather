9b8f330298637399aaa62b2b482e96909236ac2a
Harry A.M <harry.andriyan@gmail.com>
2018-12-14 14:33:30 +0700

    get city on home

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\City;
 
 class DefaultController extends Controller
 {
@@ -13,9 +14,11 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        $cities = $this->getDoctrine()->getRepository('AppBundle:City')->findBy(array(), array('id' => 'ASC'));
+
         return $this->render('index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'cities' => $cities
         ]);
     }
 }
--- src/AppBundle/Entity/City.php
+++ src/AppBundle/Entity/City.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace AppBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
 
 /**
  * City
@@ -16,22 +18,6 @@ class City
      * @var string
      */
     private $city;
-
-    /**
-     * @var string
-     */
-    private $capital;
-
-    /**
-     * @var float
-     */
-    private $lat;
-
-    /**
-     * @var float
-     */
-    private $lng;
-
 
     /**
      * Get id
@@ -65,78 +51,6 @@ class City
     public function getCity()
     {
         return $this->city;
-    }
-
-    /**
-     * Set capital
-     *
-     * @param string $capital
-     *
-     * @return City
-     */
-    public function setCapital($capital)
-    {
-        $this->capital = $capital;
-
-        return $this;
-    }
-
-    /**
-     * Get capital
-     *
-     * @return string
-     */
-    public function getCapital()
-    {
-        return $this->capital;
-    }
-
-    /**
-     * Set lat
-     *
-     * @param float $lat
-     *
-     * @return City
-     */
-    public function setLat($lat)
-    {
-        $this->lat = $lat;
-
-        return $this;
-    }
-
-    /**
-     * Get lat
-     *
-     * @return float
-     */
-    public function getLat()
-    {
-        return $this->lat;
-    }
-
-    /**
-     * Set lng
-     *
-     * @param float $lng
-     *
-     * @return City
-     */
-    public function setLng($lng)
-    {
-        $this->lng = $lng;
-
-        return $this;
-    }
-
-    /**
-     * Get lng
-     *
-     * @return float
-     */
-    public function getLng()
-    {
-        return $this->lng;
     }
 }
 
