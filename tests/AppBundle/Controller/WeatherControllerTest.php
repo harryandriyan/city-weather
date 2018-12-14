b90934035af4c384f70f44b902259b3a6c59b076
Harry A.M <harry.andriyan@gmail.com>
2018-12-14 16:56:46 +0700

    add weather test

--- tests/AppBundle/Controller/WeatherControllerTest.php
+++ tests/AppBundle/Controller/WeatherControllerTest.php
@@ -6,4 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class WeatherControllerTest extends WebTestCase
 {
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/weather?city=Sydney');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
 }
