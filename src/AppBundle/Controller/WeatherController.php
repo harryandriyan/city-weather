1a39f3e97a583a81a7a3fbf570715eeb6d0cbabf
Harry A.M <harry.andriyan@gmail.com>
2018-12-14 23:08:16 +0700

    add near city weather info

--- src/AppBundle/Controller/WeatherController.php
+++ src/AppBundle/Controller/WeatherController.php
@@ -15,8 +15,22 @@ class WeatherController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $client = new Client();
         $city = $request->query->get('city');
+        $city_weather = $this->getCityWeather($city);
+
+        $lat = $city_weather['coord']['lat'];
+        $lon = $city_weather['coord']['lon'];
+        $near_weather = $this->getNearCityWeather($lat, $lon);
+
+        return $this->render('weather.html.twig', [
+            'city_weather' => $city_weather,
+            'near_weather' => $near_weather
+        ]);
+    }
+
+    protected function getCityWeather($city)
+    {
+        $client = new Client();
         $uri = 'http://api.openweathermap.org/data/2.5/weather?q='.$city.',au&units=metric&appid=5817b972e7e6c3386e8c3e0d4d937f9a';
         $response = $client->request('GET', $uri);
 
@@ -26,8 +40,22 @@ class WeatherController extends Controller
             $city_weather = json_decode($body->getContents(), true);
         }
 
-        return $this->render('weather.html.twig', [
-            'city_weather' => $city_weather
-        ]);
+        return $city_weather;
+    }
+
+    protected function getNearCityWeather($lat, $lon)
+    {
+        $client = new Client();
+        $uri = 'http://api.openweathermap.org/data/2.5/find?lat='.$lat.'&lon='.$lon.'&cnt=16&units=metric&appid=5817b972e7e6c3386e8c3e0d4d937f9a';
+        $response = $client->request('GET', $uri);
+
+        $near_weather = [];
+        if ($response->hasHeader('Content-Length')) {
+            $body = $response->getBody();
+            $near_weather = json_decode($body->getContents(), true);
+            unset($near_weather['list'][0]);
+        }
+
+        return $near_weather['list'];
     }
 }
