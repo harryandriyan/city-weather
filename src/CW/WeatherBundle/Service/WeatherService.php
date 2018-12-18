a8763541615791ba161d42d932a6dc4bd8686f9c
Harry A.M <harry.andriyan@gmail.com>
2018-12-18 17:27:05 +0700

    implement service in separete bundle

--- src/CW/WeatherBundle/Service/WeatherService.php
+++ src/CW/WeatherBundle/Service/WeatherService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle\Service;
+namespace CW\WeatherBundle\Service;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
---
+++ src/CW/WeatherBundle/WeatherBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CW\WeatherBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WeatherBundle extends Bundle
+{
+}
