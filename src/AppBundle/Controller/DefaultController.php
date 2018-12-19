a4cab2a9c62246fa69e06beddfe330b35426f54b
Harry A.M <harry.andriyan@gmail.com>
2018-12-19 12:17:17 +0700

    implement entity manager in config

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,8 +5,6 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Doctrine\ORM\Query;
-use AppBundle\Entity\City;
 
 class DefaultController extends Controller
 {
@@ -16,15 +14,19 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         $base_dir = realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR;
+        $em = $this->get('doctrine')->getManager();
+        $cities = $em->getRepository("AppBundle:City")->findAll();
+        $listCity = [];
 
-        $query = $this->getDoctrine()
-            ->getRepository(City::class)
-            ->createQueryBuilder('c')
-            ->getQuery();
-        $cities = $query->getResult(Query::HYDRATE_ARRAY);
+        foreach ($cities as $list) {
+            $listCity[] = [
+                'id' => $list->getId(),
+                'city' => $list->getCity()
+            ];
+        }
 
         return $this->render('index.html.twig', [
-            'cities' => $cities,
+            'cities' => $listCity,
             'base_dir' => $base_dir
         ]);
     }
